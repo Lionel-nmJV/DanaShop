3e2ce7d5c9659abaed04e63a08f273f79758913d
Darul Ikhsan <darulikhssan@gmail.com>
2023-10-16 20:15:57 +0800

    add product id on response data list product

--- domain/product/repository.go
+++ domain/product/repository.go
@@ -14,7 +14,7 @@ func newRepoProduct() repoProduct {
 }
 
 func (r repoProduct) findAllByMerchantID(ctx *gin.Context, tx *sqlx.Tx, merchantID string, query string, limit interface{}, offset int) ([]productResponses, error) {
-	SQL := `SELECT "name", "category", "price", "stock", "image_url" FROM "products" WHERE "merchant_id"=$1 AND "name"  ILIKE CONCAT('%', $2::text, '%') LIMIT $3 OFFSET $4`
+	SQL := `SELECT "id", "name", "category", "price", "stock", "image_url" FROM "products" WHERE "merchant_id"=$1 AND "name"  ILIKE CONCAT('%', $2::text, '%') LIMIT $3 OFFSET $4`
 	rows, err := tx.QueryContext(ctx, SQL, merchantID, query, limit, offset)
 	if err != nil {
 		return nil, err
@@ -24,7 +24,7 @@ func (r repoProduct) findAllByMerchantID(ctx *gin.Context, tx *sqlx.Tx, merchant
 	var products []productResponses
 	for rows.Next() {
 		product := productResponses{}
-		err := rows.Scan(&product.Name, &product.Category, &product.Price, &product.Stock, &product.ImageURL)
+		err := rows.Scan(&product.ID, &product.Name, &product.Category, &product.Price, &product.Stock, &product.ImageURL)
 		if err != nil {
 			return nil, err
 		}
