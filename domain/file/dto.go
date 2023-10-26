057576635efb6d4b98b14aa25c2aed3c4617d0f5
Darul Ikhsan <darulikhssan@gmail.com>
2023-10-26 20:22:41 +0800

    fix file uploda validation

--- domain/file/dto.go
+++ domain/file/dto.go
@@ -4,6 +4,6 @@ import "mime/multipart"
 
 type requestUploadFile struct {
 	File *multipart.FileHeader `json:"file"validate:"required"`
-	Type string                `json:"type"validate:"required,oneof=product merchant campaign"`
+	Type string                `json:"type"validate:"required,oneof=product profile campaign"`
 	Ext  string                `json:"ext"validate:"required,oneof=png jpg jpeg webp mp4"`
 }
